dc34df5c152fb63bdc86f98d3b8034b1a364eaa4
lambertse <lambert.software.engineer@gmail.com>
2025-08-11 02:49:28 +0000

    Config transparency for lavi

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,12 @@
 return {
     'b0o/lavi.nvim',
     dependencies = {'rktjmp/lush.nvim'},
-    config = function() vim.cmd [[colorscheme lavi]] end
+    config = function()
+        vim.cmd [[colorscheme lavi]]
+        vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+        vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+        vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
+        vim.cmd("hi SignColumn guibg=NONE ctermbg=NONE")
+        vim.cmd("hi VertSplit guibg=NONE ctermbg=NONE")
+    end
 }
