48925748c443ed7415dde62cfa1757be1b7f5fae
Minh-Tri Le <lambert.software.engineer@gmail.com>
2025-08-11 20:48:40 +0700

    Config nvim-lint

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,18 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = {"BufReadPost", "BufWritePost"},
+    config = function()
+        local lint = require("lint")
+
+        -- Configure linters per filetype
+        lint.linters_by_ft = {
+            cpp = {"clangtidy", "cppcheck"} -- or "cppcheck"
+        }
+
+        -- Auto-run lint on save or text change
+        vim.api.nvim_create_autocmd({
+            "BufWritePost", "BufReadPost", "InsertLeave"
+        }, {callback = function() lint.try_lint() end})
+    end
+}
+
